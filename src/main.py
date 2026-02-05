4b881dc4717f4e4896f4a8678124821330288494
zhou-xiao-chuan <zhouxc@hotmail.com>
2026-02-05 19:43:01 +0800

    -

--- src/main.py
+++ src/main.py
@@ -10,12 +10,6 @@ import rc.finwise_rc
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
-
-    # # 获取当前脚本所在目录
-    # current_dir = os.path.dirname(os.path.abspath(__file__))
-    # # 设置应用程序级别的默认图标（所有窗口默认使用）
-    # app.setWindowIcon(QIcon(os.path.join(current_dir, "rc/logo.ico")))
-
     app.setWindowIcon(QIcon(":/images/logo.ico"))
 
     mainWindow = MainWindow()
